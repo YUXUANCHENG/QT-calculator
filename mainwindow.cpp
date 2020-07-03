e4ebbf5380069f8489abe932e563e71ed379f8bd
yuxuancheng <yuxuancheng@outlook.com>
2020-07-03 19:58:34 -0400

    fixed a bug

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,15 +8,15 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->pushButton_1,SIGNAL(released()),this,SLOT(press_digit));
-    connect(ui->pushButton_2,SIGNAL(released()),this,SLOT(press_digit));
-    connect(ui->pushButton_3,SIGNAL(released()),this,SLOT(press_digit));
-    connect(ui->pushButton_4,SIGNAL(released()),this,SLOT(press_digit));
-    connect(ui->pushButton_5,SIGNAL(released()),this,SLOT(press_digit));
-    connect(ui->pushButton_6,SIGNAL(released()),this,SLOT(press_digit));
-    connect(ui->pushButton_7,SIGNAL(released()),this,SLOT(press_digit));
-    connect(ui->pushButton_8,SIGNAL(released()),this,SLOT(press_digit));
-    connect(ui->pushButton_9,SIGNAL(released()),this,SLOT(press_digit));
+    connect(ui->pushButton_1,SIGNAL(released()),this,SLOT(press_digit()));
+    connect(ui->pushButton_2,SIGNAL(released()),this,SLOT(press_digit()));
+    connect(ui->pushButton_3,SIGNAL(released()),this,SLOT(press_digit()));
+    connect(ui->pushButton_4,SIGNAL(released()),this,SLOT(press_digit()));
+    connect(ui->pushButton_5,SIGNAL(released()),this,SLOT(press_digit()));
+    connect(ui->pushButton_6,SIGNAL(released()),this,SLOT(press_digit()));
+    connect(ui->pushButton_7,SIGNAL(released()),this,SLOT(press_digit()));
+    connect(ui->pushButton_8,SIGNAL(released()),this,SLOT(press_digit()));
+    connect(ui->pushButton_9,SIGNAL(released()),this,SLOT(press_digit()));
 }
 
 MainWindow::~MainWindow()
